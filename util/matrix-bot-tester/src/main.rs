444a81c2ec35a671ed9dfed53e01d15e44c5d2d3
Timothy Hobbs <timothyhobbs@seznam.cz>
2022-06-14 17:44:07 +0000

    Get matrix bot tester building and fix error handling code
    Also flesh out pseudocode

--- util/matrix-bot-tester/src/main.rs
+++ util/matrix-bot-tester/src/main.rs
@@ -2,6 +2,7 @@ extern crate config;
 
 use matrix_bot_tester::args::{Args};
 use clap::Parser;
+use std::process::{exit};
 
 use matrix_sdk::{
     self,
@@ -20,14 +21,22 @@ async fn main() -> Result<(), matrix_sdk::Error>  {
     let args = Args::parse();
 
     let mut settings = config::Config::default();
-    settings
-        .merge(config::File::with_name(&args.bot_config))
-        .unwrap();
+    match settings.merge(config::File::with_name(&args.bot_config)) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Error loading settings file: {}", e);
+                exit(1);
+            }
+    };
 
     let mut replay = config::Config::default();
-    settings
-        .merge(config::File::with_name(&args.replay))
-        .unwrap();
+    match replay.merge(config::File::with_name(&args.replay)){
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("Error loading replay file: {}", e);
+            exit(1);
+        }
+    };
 
     // Login
     let user = settings.get_str("user").unwrap();
@@ -36,26 +45,33 @@ async fn main() -> Result<(), matrix_sdk::Error>  {
     let homeserver_url = Url::parse(&homeserver_url).expect("Couldn't parse the homeserver URL");
     let client = Client::new(homeserver_url).unwrap();
 
-    client.register_event_handler(on_room_message).await;
+    // client.register_event_handler(on_room_message).await;
 
     client.login(&user, &password, None, None).await;
-    client.sync(SyncSettings::new()).await;
+    //client.sync(SyncSettings::new()).await;
+    return Ok(());
 
+    /*
+    let mut room = bot.create_and_join_room(inviting=[settings.get_str("user_to_test").unwrap()], private=true);
     bot.run(&user, &password, &homeserver_url);
 
     let messages = replay.get_array("messages").unwrap();
     for message in messages {
         let message_pair = message.into_table().unwrap();
-        let send = message_pair.get("send").unwrap();
-        println!("send: {}", send);
-        bot.send_to_user(message_pair.get("send").unwrap(), args.user_to_test);
-        let response = bot.recieve_from_user(args.user_to_test);
-        println!("recieved: {}", response);
-        let expectation = message_pair.get("expect").unwrap();
-        if response != expectation {
-            eprintln!("Expected to hear '{}'", expectation);
-            std::process::exit(1);
-        }
+        if let send = message_pair.get("send") {
+            println!("send: {}", send);
+            bot.send_to_user(message_pair.get("send").unwrap(), args.user_to_test);
+        };
+        if let expectation = message_pair.get("expect"){
+            let response = bot.recieve_from_user(args.user_to_test);
+            println!("recieved: {}", response);
+            if response != expectation {
+                eprintln!("Expected to hear '{}'", expectation);
+                std::process::exit(1);
+            }
+
+        };
     }
+*/
 
 }
